58cc318b4d71a36c5f10e877ac74e40880adac7a
Omars.DEV <omarsukareih99@gmail.com>
2022-05-11 09:27:14 +0300

    remove main view

--- App.js
+++ App.js
@@ -3,13 +3,11 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import MainBottomTabs from './src/stack/MainBottomTabs';
-import MainView from './src/screen/MainView';
 
 const App = () => {
   return (
     <NavigationContainer>
       <MainBottomTabs />
-      {/* <MainView /> */}
     </NavigationContainer>
   );
 };
